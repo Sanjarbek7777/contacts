cc2f53fda0c128aecf2ea4ed6291aab71bb63042
Sanjarbek7777 <rsanjarbek450@gmail.com>
2021-09-07 16:47:28 +0500

    Add pokemons

---
+++ js/pokemon.js
@@ -0,0 +1,19 @@
+var pokemonsList = document.querySelector('.pokemons-list');
+
+var pokemonsItemTemplate = document.querySelector('#pokemons-item-template').content;
+var pokemons = pokemons;
+
+var pokemonsListFragment = document.createDocumentFragment();
+
+for (var pokemon of pokemons) {
+  var elPokemon = pokemonsItemTemplate.cloneNode(true);
+
+  elPokemon.querySelector('.pokemon__img').src = pokemon.img;
+  elPokemon.querySelector('.pokemon__name').textContent = pokemon.name;
+  elPokemon.querySelector('.pokemon__number').textContent = '#' + pokemon.num;
+
+
+  pokemonsListFragment.appendChild(elPokemon);
+}
+
+pokemonsList.appendChild(pokemonsListFragment);
