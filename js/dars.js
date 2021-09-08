fd75d1147d33e1a1e6a8ee7dd13bf88a31e9e058
Sanjarbek7777 <rsanjarbek450@gmail.com>
2021-09-08 16:33:27 +0500

    Add delete button

--- js/dars.js
+++ js/dars.js
@@ -4,78 +4,85 @@ var contacts = [];
 
 // DOM
 var elNewContactForm = document.querySelector('.js-new-contact-form');
+var elNewContactNameInput = elNewContactForm.querySelector('.js-new-contact-name-input');
+var elNewContactRelationshipInput = elNewContactForm.querySelector('.js-new-contact-relationship-input');
+var elNewContactPhoneInput = elNewContactForm.querySelector('.js-new-contact-phone-input');
 
 var elContacts = document.querySelector('.contacts');
 var elContactsList = elContacts.querySelector('.contacts__list');
 
-var isNumberExist = elNewContactForm.querySelector('.isNumberExist');
+
+
+function addContact() {
+  // Add contact to contacts array
+  contacts.push({
+  name: elNewContactNameInput.value.trim(),
+  relationship: elNewContactRelationshipInput.value.trim(),
+  phone: elNewContactPhoneInput.value.trim()
+  });
+
+      // Reset inputs
+      elNewContactNameInput.value = '';
+      elNewContactRelationshipInput.value = '';
+      elNewContactPhoneInput.value = '';
+};
+
+function showContacts(){
+  elContactsList.innerHTML = '';
+  var elContactsFragment = document.createDocumentFragment();
+
+  var elContactsTemplate = document.querySelector('#contacts-template').content;
+  contacts.forEach(function (contact, index) {
+    var elContactItem = elContactsTemplate.cloneNode(true);
+
+    elContactItem.querySelector('.contact-title').textContent = contact.name;
+    elContactItem.querySelector('.contact-relationship ').textContent = contact.relationship;
+    elContactItem.querySelector('.contact-phone').textContent = contact.phone;
+    elContactItem.querySelector('.contact-phone').href = 'tel:' + (contact.phone);
+    elContactItem.querySelector('.contact-phone').dataset.index = index;
+
+    elContactsFragment.appendChild(elContactItem);
+  })
+
+  elContactsList.appendChild(elContactsFragment);
+
+
+}
 
 // Form bo'lsa
 if (elNewContactForm) {
   elNewContactForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
-    var elNewContactNameInput = elNewContactForm.querySelector('.js-new-contact-name-input');
-    var elNewContactRelationshipInput = elNewContactForm.querySelector('.js-new-contact-relationship-input');
-    var elNewContactPhoneInput = elNewContactForm.querySelector('.js-new-contact-phone-input');
 
-    var isAvailable = contacts.findIndex(function(contact){
-      return contact.phone === elNewContactPhoneInput.value.trim();
-    })
-    console.log(isAvailable);
 
-    if(isAvailable > -1){
-      elNewContactPhoneInput.classList.add('is-invalid');
-      isNumberExist.classList.remove('d-none');
+    var find = contacts.findIndex(function (tell) {
+        return tell.phone === elNewContactPhoneInput.value;
+      });
+
+    if (find > -1) {
+      elNewContactPhoneInput.classList.add('is-invalid')
       return;
-    }else{
-      elNewContactPhoneInput.classList.remove('is-invalid');
-      isNumberExist.classList.add('d-none');
+    }else {
+      elNewContactPhoneInput.classList.remove('is-invalid')
     }
 
     addContact();
+
     showContacts();
-
-    function addContact(){
-      contacts.push({
-        name: elNewContactNameInput.value.trim(),
-        relationship: elNewContactRelationshipInput.value.trim(),
-        phone: elNewContactPhoneInput.value.trim()
-      });
-
-      elNewContactNameInput.value = '';
-      elNewContactRelationshipInput.value = '';
-      elNewContactPhoneInput.value = '';
-    }
-
-    function showContacts(){
-      elContactsList.innerHTML = '';
-      var elContactsFragment = document.createDocumentFragment();
-
-      for (var contact of contacts) {
-        var elNewLi = document.createElement('li');
-        elNewLi.classList.add('list-group-item', 'p-3');
-
-        var elNewHeading = document.createElement('h3');
-        elNewHeading.classList.add('h5');
-        elNewHeading.textContent = contact.name;
-        elNewLi.appendChild(elNewHeading);
-
-        var elNewRelDiv = document.createElement('div');
-        elNewRelDiv.classList.add('mb-2');
-        elNewRelDiv.textContent = contact.relationship;
-        elNewLi.appendChild(elNewRelDiv);
-
-        var elNewPhoneLink = document.createElement('a');
-        elNewPhoneLink.classList.add('btn', 'btn-outline-success', 'btn-sm');
-        elNewPhoneLink.href = 'tel:' + contact.phone;
-        elNewPhoneLink.textContent = contact.phone;
-        elNewLi.appendChild(elNewPhoneLink);
-
-        elContactsFragment.appendChild(elNewLi);
-      }
-      elContactsList.appendChild(elContactsFragment);
-    }
-
   });
 }
+// Form submitda amal bajariladi
+
+function deletecontact(index) {
+  contacts.splice(index, 1);
+}
+
+if (elContactsList) {
+  elContactsList.addEventListener('click', function(evt) {
+    if (evt.target.matches('.contact-btn')) {
+      deletecontact(Number(evt.target.dataset.index));
+      showContacts();
+    };
+  });
+};
